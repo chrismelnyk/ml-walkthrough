17eeddbbbbc3567ac573de466debfa03258e0904
Chris Melnyk <xplowersu@gmail.com>
2018-09-08 15:31:52 -0400

    code 1

--- part1.py
+++ part1.py
@@ -1,1 +1,30 @@
 from __future__ import division, print_function, unicode_literals
+from IPython import get_ipython
+import numpy as np
+import os
+
+np.random.seed(42)
+
+from IPython import get_ipython
+ipy = get_ipython()
+if ipy is not None:
+    ipy.run_line_magic('matplotlib', 'inline')
+
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "fundamentals"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+import warnings
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
